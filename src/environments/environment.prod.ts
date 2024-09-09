8ca4b8063e415832938208a8c36f3659db379814
sumantac <talktosumanta@gmail.com>
2024-09-09 11:54:11 -0400

    firebase dev database link corrected for ticket booking and other transaction during tetsing.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -24,8 +24,10 @@ export const environment = {
     apiKey: "AIzaSyCxKE2SsBVjG-RA1uv54KgcFI6-Z0-GdKc",
     authDomain: "nvba-6bd35.firebaseapp.com",
     databaseURL: "https://nvba-6bd35.firebaseio.com",
-    projectId: "nvba-6bd35",
-    storageBucket: "nvba-6bd35.appspot.com",
+    projectId: "nvba-dev",
+    storageBucket: "nvba-dev.appspot.com",
+    // projectId: "nvba-6bd35",
+    // storageBucket: "nvba-6bd35.appspot.com",
     messagingSenderId: "576609571271",
     appId: "1:576609571271:web:d7648e6a22632a55ef49e2",
     measurementId: "G-YM094H77Y4"
