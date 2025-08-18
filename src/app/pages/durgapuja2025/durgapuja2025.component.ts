61dba26ab2ce3937eec8b85891936f2dcd9db31f
sumantac <sumanta.chakraborty@rfidgs.com>
2025-08-18 10:41:30 -0400

    Merge branch 'production' into development

--- src/app/pages/durgapuja2025/durgapuja2025.component.ts
+++ src/app/pages/durgapuja2025/durgapuja2025.component.ts
@@ -1,15 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ElementRef, ViewChild, OnDestroy } from '@angular/core';
+import { GetjsonfileService } from './../../services/getjsonfile.service';
+import { AuthService } from './../../shared/services/auth.service';
 
 @Component({
   selector: 'app-durgapuja2025',
   templateUrl: './durgapuja2025.component.html',
   styleUrls: ['./durgapuja2025.component.scss']
 })
-export class Durgapuja2025Component implements OnInit {
+export class Durgapuja2025Component implements OnInit, OnDestroy {
 
-  constructor() { }
+    @ViewChild('playA') playA!: ElementRef;
+
+  sliderImage : any;
+  playAudio: boolean = true;
+  audio = new Audio();
+  isLog: boolean = true;
+
+  constructor( private jsonFile:GetjsonfileService, public auth: AuthService ) {
+      
+    console.log(this.auth.isLoggedIn.valueOf());
+}
 
   ngOnInit(): void {
+  }
+
+    ngOnDestroy(): void {
+    this.audio.pause();
+    console.log('Pause');
+    this.playAudio = false;
+  }
+
+    soundplay() : void{
+    if(this.playAudio == false){
+      this.audio.play();
+      console.log('Play');
+      this.playAudio = false;
+    }
+    else{
+      this.audio.pause();
+      console.log('Pause');
+      this.playAudio = false;
+    }
   }
 
 }
