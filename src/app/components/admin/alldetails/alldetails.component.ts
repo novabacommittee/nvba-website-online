47762b164c4a2fbb901e5ab838dca2fb4d2e1bb6
sumantac <sumanta.chakraborty@rfidgs.com>
2025-09-19 01:53:18 -0400

    Admin report updated for small fix in concert ticket

--- src/app/components/admin/alldetails/alldetails.component.ts
+++ src/app/components/admin/alldetails/alldetails.component.ts
@@ -349,9 +349,9 @@ export class AlldetailsComponent implements OnInit {
           }
        });
 
-       Object.assign(concertPurchase,{ create_time: moment(ct.create_time).format("YYYY-MM-DD HH:mm"), first_name:ct.payer.payer_info.first_name, 
-        last_name:ct.payer.payer_info.last_name,
-        email:ct.payer.payer_info.email,description:ct.purchase_units[0].items[0].name,
+       Object.assign(concertPurchase,{ create_time: moment(ct.create_time).format("YYYY-MM-DD HH:mm"), first_name:ct.payer.name.given_name, 
+        last_name:ct.payer.name.surname,
+        email:ct.payer.email_address,description:ct.purchase_units[0].items[0].name,
         price:ct.purchase_units[0].amount.value,quantity:ct.purchase_units[0].items[0].quantity,sku:ct.purchase_units[0].items[0].sku});
 
       this.dp2025ConcertTicketList.unshift(concertPurchase);
