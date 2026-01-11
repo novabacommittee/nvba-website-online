c4f1b1f8ad9ff0760df9b2a4a786439ea6a07a13
arijitpal <arijitpal.us@gmail.com>
2026-01-11 15:24:52 -0500

    Fix logout issue on clicking current event

--- src/app/pages/contactus/contactus.component.ts
+++ src/app/pages/contactus/contactus.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { AuthService } from './../../shared/services/auth.service';
-import { MemberService } from './../../shared/member/member.service';
+import {Component, OnInit} from '@angular/core';
+import {AuthService} from './../../shared/services/auth.service';
+import {MemberService} from './../../shared/member/member.service';
 
 @Component({
   selector: 'app-contactus',
@@ -8,60 +8,57 @@ import { MemberService } from './../../shared/member/member.service';
   styleUrls: ['./contactus.component.scss']
 })
 export class ContactusComponent implements OnInit {
-  member:any;
-  constructor( public auth: AuthService, public memberService: MemberService ) { }
+  member: any;
 
-  ngOnInit(): void {
-      this.member = JSON.parse(localStorage.getItem('user')!);
-      ////console.log(this.member);
-      ////console.log(this.member.uid);
-
-      // Only proceed if member exists
-      if (this.member) {
-        this.datafind();
-        this.datafind1();
-      }
+  constructor(public auth: AuthService, public memberService: MemberService) {
   }
 
-  datafind(){
-      // this.memberService.memberRef.snapshotChanges().subscribe(m =>{
-      //    let d =  m.payload.toJSON();
-      //   ////console.log(d);
-      // })
-      ////console.log(this.member.email);
-      if (this.member && this.member.email) {
-        this.memberService.GetMember("198").snapshotChanges().subscribe(m =>{
-          let memberData = m.payload.toJSON()
-          ////console.log(memberData);
-          this.member = memberData;
-          ////console.log('this.member - ');
-          ////console.log(this.member.email);
-        })
-        this.memberService.GetMemberbyEmail(this.member.email).snapshotChanges().subscribe(mm =>{
-          let md = mm.forEach(mmd=>{
-          let s =  mmd.payload.toJSON()
-          ////console.log( s?.valueOf())
-          
-            // if( s.email == this.member.email){
+  ngOnInit(): void {
+    this.member = JSON.parse(localStorage.getItem('user')!);
+    ////console.log(this.member);
+    ////console.log(this.member.uid);
 
-            // }
-          })
-          // ////console.log(memberData);
-          // this.member = memberData;
-          // ////console.log('this.member - ');
-          // ////console.log(this.member);
-        })
-      }
-
-
+    // Only proceed if member exists
+    if (this.member) {
+      this.datafind();
+      this.datafind1();
     }
+  }
 
-   async datafind1(){
+  datafind() {
+    // this.memberService.memberRef.snapshotChanges().subscribe(m =>{
+    //    let d =  m.payload.toJSON();
+    //   ////console.log(d);
+    // })
+    ////console.log(this.member.email);
+    if (this.member && this.member.email) {
+      this.memberService.GetMember("198").snapshotChanges().subscribe(m => {
+        let memberData = m.payload.toJSON()
+        ////console.log(memberData);
+        this.member = memberData;
+        ////console.log('this.member - ');
+        ////console.log(this.member.email);
+      })
+      this.memberService.GetMemberbyEmail(this.member.email).snapshotChanges().subscribe(mm => {
+        let md = mm.forEach(mmd => {
+          let s = mmd.payload.toJSON()
+          ////console.log( s?.valueOf())
 
-     //  ////console.log( await this.auth.memberDetail());
+          // if( s.email == this.member.email){
 
-   }
+          // }
+        })
+        // ////console.log(memberData);
+        // this.member = memberData;
+        // ////console.log('this.member - ');
+        // ////console.log(this.member);
+      })
+    }
+  }
 
+  async datafind1() {
+    //  ////console.log( await this.auth.memberDetail());
+  }
 
 
 }
