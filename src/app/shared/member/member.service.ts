a0f52dd4e8bc3b5d8d4b3cbbf0f6dcaa5d39c231
sumantac <sumanta.chakraborty@rfidgs.com>
2026-01-18 20:35:12 -0500

    auth code change reverted back in dev

--- src/app/shared/member/member.service.ts
+++ src/app/shared/member/member.service.ts
@@ -23,32 +23,19 @@ export class MemberService {
   }
 
   /* Create member */
-  //AddMember(member: any) {
-  AddMember(uid: string, email: string) {
+  AddMember(member: any) {
     // this.membersRef
     //   .push({member})
     //   .catch((error) => {
     //     this.errorMgmt(error);
     //   });
-
-    return this.db.object(`/Members/${uid}`).set({
-      uid,
-      email,
-      createdAt: Date.now()
-    })
-    .then(() => console.log('Success'))
-    .catch(err => {
-      this.errorMgmt(err);
+    this.db.object('/Members/'+member.id).set({ ...member }).catch(error => 
+    {
+      ////console.log(error);
+    }).then( c => {
+      ////console.log("Success Create.");
     });
-
-    // this.db.object('/Members/'+member.id).set({ ...member }).catch(error => 
-    // {
-    //   ////console.log(error);
-    // }).then( c => {
-    //   ////console.log("Success Create.");
-    // });
   }
-
   /* Get member */
   GetMember(id: string) {
     this.memberRef = this.db.object('/Members/' + id);
@@ -58,19 +45,13 @@ export class MemberService {
   /* Get member */
   GetMemberbyEmail(email: string) {
     this.membersRef = this.db.list('/Members'+email);
-
-    // return this.db.list('/Members', ref =>
-    //   ref.orderByChild('email').equalTo(email)
-    // ).valueChanges();
-    //   this.membersRef.valueChanges()
+ //   this.membersRef.valueChanges()
     return  this.membersRef;
   }
 
   /* Get members list */
   GetMembersList() {
   //  this.membersRef = this.db.list('/Members');
-    //return this.db.list('/Members').valueChanges();
-    
     return this.items;
   }
 
@@ -84,11 +65,12 @@ export class MemberService {
     //     this.errorMgmt(error);
     //   });
 
-    this.db.object('/Members/' + memb.id).update( JSON.parse( JSON.stringify(memb ) ))
-      .then( c => {console.log("success Update");})
-      .catch(error => {
-        this.errorMgmt(error);
-      });
+    this.db.object('/Members/' + memb.id).update( JSON.parse( JSON.stringify(memb ) )).catch(error => {
+      this.errorMgmt(error);
+      console.log(error);
+    }).then( c => {
+      console.log("success Update");
+    });
   }
 
   /* Delete Member */
@@ -98,42 +80,41 @@ export class MemberService {
   //     this.errorMgmt(error);
   //   });
   // }
-
   // Error management
   private errorMgmt(error: any) {
-    console.log(error);
-    throw error;
+    ////console.log(error);
   }
+
 
   concert(purches:any){
-    this.db.object('/concert-2025/'+purches.id).set({ ...purches }).then( c => {
-      console.log("Success Create.");
-    }).catch(error => {
-      this.errorMgmt(error);
-    });
-  }
+      this.db.object('/concert-2025/'+purches.id).set({ ...purches }).catch(error => {
+            console.log(error);
+          }).then( c => {
+            console.log("Success Create.");
+          });
+      }
   
   kp2023(purches:any){
-    this.db.object('/kp2023/'+purches.id).set({ ...purches }).then( c => {
-      console.log("Success Create.");
-    }).catch(error => {
-      this.errorMgmt(error);
-    });
-  }
+        this.db.object('/kp2023/'+purches.id).set({ ...purches }).catch(error => {
+              console.log(error);
+            }).then( c => {
+              console.log("Success Create.");
+            });
+        }
 
   kp2024(purches:any){
-    this.db.object('/kp2024/'+purches.id).set({ ...purches }).then( c => {
-      console.log("Success Create.");
-    }).catch(error => {
-      this.errorMgmt(error);
-    });
-  }
+        this.db.object('/kp2024/'+purches.id).set({ ...purches }).catch(error => {
+              console.log(error);
+            }).then( c => {
+              console.log("Success Create.");
+            });
+        }
 
   kp2025(purches:any){
-    this.db.object('/kp2025/'+purches.id).set({ ...purches }).then( c => {
-        console.log("Success Create.");
-      }).catch(error => {
-        this.errorMgmt(error);
-      });
-  }
+    this.db.object('/kp2025/'+purches.id).set({ ...purches }).catch(error => {
+          console.log(error);
+        }).then( c => {
+          console.log("Success Create.");
+        });
+    }
 }
