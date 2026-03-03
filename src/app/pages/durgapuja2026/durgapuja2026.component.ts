8356914dc93661dd985da1c03a430ec7bb46f1ec
novabacommittee <novabacommittee@yahoo.com>
2026-03-02 21:17:42 -0500

    Added new pages for 2026

--- src/app/pages/durgapuja2026/durgapuja2026.component.ts
+++ src/app/pages/durgapuja2026/durgapuja2026.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, AfterViewInit, ElementRef, ViewChild, OnDestroy } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { GetjsonfileService } from './../../services/getjsonfile.service';
 import { AuthService } from './../../shared/services/auth.service';
 
@@ -7,40 +7,16 @@ import { AuthService } from './../../shared/services/auth.service';
   templateUrl: './durgapuja2026.component.html',
   styleUrls: ['./durgapuja2026.component.scss']
 })
-export class Durgapuja2026Component implements OnInit, OnDestroy {
-
-    @ViewChild('playA') playA!: ElementRef;
+export class Durgapuja2026Component implements OnInit {
 
   sliderImage : any;
-  playAudio: boolean = true;
-  audio = new Audio();
   isLog: boolean = true;
 
   constructor( private jsonFile:GetjsonfileService, public auth: AuthService ) {
-
     console.log(this.auth.isLoggedIn.valueOf());
-}
+  }
 
   ngOnInit(): void {
-  }
-
-    ngOnDestroy(): void {
-    this.audio.pause();
-    console.log('Pause');
-    this.playAudio = false;
-  }
-
-    soundplay() : void{
-    if(this.playAudio == false){
-      this.audio.play();
-      console.log('Play');
-      this.playAudio = false;
-    }
-    else{
-      this.audio.pause();
-      console.log('Pause');
-      this.playAudio = false;
-    }
   }
 
 }
