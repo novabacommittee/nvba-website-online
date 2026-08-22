49ac283c8e2edf5021724ebca767fe368536dec8
novabacommittee <novabacommittee@yahoo.com>
2026-08-22 00:14:22 -0400

    Dev env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -21,37 +21,19 @@
 // };
 
 export const environment = {
-  production: true,
+  production: false,
   firebase: {
-    apiKey: "AIzaSyDR90ComUId7DKFdXumxWXTfbYFKeH_neg",
-    authDomain: "novaba.firebaseapp.com",
-    databaseURL: "https://novaba.firebaseio.com",
-    projectId: "novaba",
-    storageBucket: "novaba.appspot.com",
-    messagingSenderId: "253964137737",
-    appId: "1:253964137737:web:f438052db1faa48568cfd8",
-    measurementId: "G-96GP6RS4JF",
+    apiKey: "AIzaSyCxKE2SsBVjG-RA1uv54KgcFI6-Z0-GdKc",
+    authDomain: "nvba-6bd35.firebaseapp.com",
+    databaseURL: "https://nvba-6bd35.firebaseio.com",
+    projectId: "nvba-6bd35",
+    storageBucket: "nvba-6bd35.appspot.com",
+    messagingSenderId: "576609571271",
+    appId: "1:576609571271:web:d7648e6a22632a55ef49e2",
+    measurementId: "G-YM094H77Y4"
   },
   paypal:{
-    clientId: 'AVBsfj0Jw-jl5_63BPGwuduCaKDsPvbz1pwyqECm7N5FzKEi1Q_o-xQAiM_BTzQhAW064uAPf1v9uZdS',
+    clientId: 'AeLhWUCfC2jHOZv7b-KDfZV6R6Mig-2FklW6iIxsuI0UROww652TU9SlVPHyW1ygMGohQo21TfXUVPrz',
     currency: 'USD'
   }
 };
-
-// export const environment = {
-//   production: false,
-//   firebase: {
-//     apiKey: "AIzaSyCxKE2SsBVjG-RA1uv54KgcFI6-Z0-GdKc",
-//     authDomain: "nvba-6bd35.firebaseapp.com",
-//     databaseURL: "https://nvba-6bd35.firebaseio.com",
-//     projectId: "nvba-6bd35",
-//     storageBucket: "nvba-6bd35.appspot.com",
-//     messagingSenderId: "576609571271",
-//     appId: "1:576609571271:web:d7648e6a22632a55ef49e2",
-//     measurementId: "G-YM094H77Y4"
-//   },
-//   paypal:{
-//     clientId: 'AeLhWUCfC2jHOZv7b-KDfZV6R6Mig-2FklW6iIxsuI0UROww652TU9SlVPHyW1ygMGohQo21TfXUVPrz',
-//     currency: 'USD'
-//   }
-// };
